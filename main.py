dc57600c4122522ac8d49382593b0f4eae9a327c
JontePonte <john_nordstrand@hotmail.com>
2018-12-19 11:09:39 +0100

    Ändra färg på bakgrund till blå

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        arcade.set_background_color(arcade.color.AMAZON)
+        # BLUE_SAPPHIRE eller BLUE_YONDER, båda är rätt snygga
+        arcade.set_background_color(arcade.color.BLUE_YONDER)
 
         # If you have sprite lists, you should create them here,
         # and set them to None
