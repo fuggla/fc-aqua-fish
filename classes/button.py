ffc939653b2fb2b4779abda6558af2848e4ad040
Fred Uggla <fred.uggla@owlnical.net>
2018-12-22 00:19:52 +0100

    Ersatte padding med outline

--- classes/button.py
+++ classes/button.py
@@ -3,13 +3,13 @@ import arcade
 
 class Button():
 
-    def __init__(self, x, y, width, height, padding_size, padding_color, background_color, text, font_size, release):
+    def __init__(self, x, y, width, height, outline_size, outline_color, background_color, text, font_size, release):
         self.x = x
         self.y = y
         self.height = height
         self.width = width
-        self.padding_size = padding_size
-        self.padding_color = padding_color
+        self.outline_size = outline_size
+        self.outline_color = outline_color
         self.background_color = background_color
         self.text = text
         self.font_size = font_size
@@ -17,8 +17,8 @@ class Button():
 
     # Rita knapp
     def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.width + self.padding_size * 2, self.height + self.padding_size * 2, self.padding_color)
         arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.background_color)
+        arcade.draw_rectangle_outline(self.x, self.y, self.width, self.height, self.outline_color, self.outline_size)
         arcade.draw_text(self.text, self.x, self.y, arcade.color.BLACK, font_size=self.font_size, width=self.width, align="center", anchor_x="center", anchor_y="center")
 
     # Kolla om angiven x y är inom knappens ramar
