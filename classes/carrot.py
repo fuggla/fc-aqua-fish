4a2485e91a176f2e1e80048195df939e4c2d9e55
JontePonte <john_nordstrand@hotmail.com>
2018-12-23 13:46:09 +0100

    Moroten sjunker

--- classes/carrot.py
+++ classes/carrot.py
@@ -23,12 +23,36 @@ class CarrotSprite(arcade.Sprite):
         self.center_x = random.randrange(sw * 0.8) + sw * 0.1
         self.center_y = random.randrange(sh * 0.8) + sh * 0.1
 
+        # Definiera variabler
+        self.change_x = 0
+        self.change_y = 0
+        self.acc_right = 0
+        self.acc_left = 0
+        self.acc_grav_float = 0             # Fulvariabel, summan av gravitationen och lyftkraften
+        self.acc_water_res = 0
+
+        self.framerate = 30         # Ska fixas sen
+        # Morotens egenskaper
+        self.size = 1
+        self.mass = 0.1
+
 
     def update(self):
         # Updatera koordinaterna innan de skickas iväg
         global carrot_cor
         carrot_cor = self.get_position()
 
+        # Beräkna acceleration i x-led
+        self.acc_right = 0
+        self.acc_left = 0
+
+        # Beräkna acceleration i y-led
+        self.acc_grav_float = - 1
+        self.acc_water_res = (self.size * self.change_y * math.fabs(self.change_y)) / self.mass
+
+        # Beräkna hastighet i x-led och y-led
+        self.change_x = self.change_x + (self.acc_right - self.acc_left) / self.framerate
+        self.change_y = self.change_y + (self.acc_grav_float - self.acc_water_res) / self.framerate
 
 
         # Anropa huvudklassen
