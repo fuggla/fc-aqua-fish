32697e9f68afcdc78c92a55e5649151b414c0d14
JontePonte <john_nordstrand@hotmail.com>
2018-12-26 23:35:27 +0100

    Klassen "FishSprite" skapas och några grundläggande metoder lades in i den

--- classes/fish.py
+++ classes/fish.py
@@ -1,1 +1,57 @@
 
+import arcade, random, math
+
+class FishSprite(arcade.Sprite):
+    def __init__(self):
+        # Anropa Sprite konstruktor
+        super().__init__()
+
+        # Definiera hastighet och acceleration
+        self.change_x = 0       # x_hastighet
+        self.change_y = 0       # y_hastighet
+        self.acc_x = 0          # positiv x_acceleration
+        self.acc_y = 0          # negativ y_acceleration
+        self.break_x = 0        # negativ x_acceleration
+        self.break_y = 0        # negativ y_acceleration
+
+    def random_move(self):
+        # Ändra accelerationen slumpartat
+        self.acc_x = (random.random() * 2 - 1) * self.finforce / self.mass
+        self.acc_y = (random.random() * 2 - 1) * self.finforce / self.mass
+
+    def chase_food(self):
+        # metod för att vända sig mot och accelerera mot mat
+        if self.food_objects:
+            food_cor = []
+            # Spara alla morätternas koordinater i food_cor
+            for food in self.food_objects:
+                food_cor.append([food.center_x, food.center_y])
+
+            # Beräkna avståndet till maten som är närmast
+            nerest_carrot = [(food_cor[0][0] - self.center_x), (food_cor[0][1] - self.center_y)]
+            for food in food_cor:
+                if ((food[0] - self.center_x) ** 2 + (food[1] - self.center_y) ** 2) < (
+                        nerest_carrot[0] ** 2 + nerest_carrot[1] ** 2):
+                    nerest_carrot = [(food[0] - self.center_x), (food[1] - self.center_y)]
+
+            # Beräkna vinkel mot maten
+            ang = math.atan2(nerest_carrot[1], nerest_carrot[0])
+            foodspeed = random.random() * self.finforce / self.mass
+            self.acc_x = foodspeed * math.cos(ang)
+            self.acc_y = foodspeed * math.sin(ang)
+
+    def check_edge(self):
+        # Kolla om fisken är nära kanten, styr in dem mot mitten och stressa upp den
+        if self.center_x > self.sw * 0.90:
+            self.acc_x = - self.finforce / self.mass
+            self.relaxed[0] = False
+        if self.center_x < self.sw * 0.10:
+            self.acc_x = self.finforce / self.mass
+            self.relaxed[0] = False
+
+        if self.center_y > self.sh * 0.90:
+            self.acc_y = - self.finforce / self.mass
+            self.relaxed[1] = False
+        if self.center_y < self.sh * 0.10:
+            self.acc_y = self.finforce / self.mass
+            self.relaxed[1] = False
--- classes/purple_fish.py
+++ classes/purple_fish.py
@@ -1,16 +1,17 @@
 
 import arcade, random, math
+from classes.fish import FishSprite
 from vars import SPRITE_SCALING_PFISH, SCREEN_WIDTH, SCREEN_HEIGHT
 
 # Klass för lila fiskar (Purple_fish)
-class PfishSprite(arcade.Sprite):
+class PfishSprite(FishSprite):
     def __init__(self, carrot_list):
         # Anropa Sprite konstruktor
         super().__init__()
 
         self.sw = SCREEN_WIDTH
         self.sh = SCREEN_HEIGHT
-        self.carrot_list = carrot_list
+        self.food_objects = carrot_list
 
         # texture 1 & 2 för höger och vänster
         self.texture_left1 = arcade.load_texture("images/purple_fish1.png", mirrored=True, scale=SPRITE_SCALING_PFISH)
@@ -32,14 +33,6 @@ class PfishSprite(arcade.Sprite):
         # Placera ut fiskarna
         self.center_x = random.randrange(self.sw * 0.8) + self.sw * 0.1
         self.center_y = random.randrange(self.sh * 0.8) + self.sh * 0.1
-
-        # Definiera hastighet och acceleration
-        self.change_x = 0       # x_hastighet
-        self.change_y = 0       # y_hastighet
-        self.acc_x = 0          # positiv x_acceleration
-        self.acc_y = 0          # negativ y_acceleration
-        self.break_x = 0        # negativ x_acceleration
-        self.break_y = 0        # negativ y_acceleration
 
         # Fiskarnas personlighet
         self.eager = 5                  # Hur ofta byter fiskarna riktning
@@ -95,50 +88,6 @@ class PfishSprite(arcade.Sprite):
 
         # Anropa huvudklassen
         super().update()
-
-
-    def chase_food(self):
-        # Om det finns morötter, de är lugna och hungriga vänder de sig mot närmaste morot
-        carrots = self.carrot_list
-        if carrots:
-            carrot_cor = []
-            # Spara alla morätternas koordinater i carrot_cor
-            for carrot in carrots:
-                carrot_cor.append([carrot.center_x, carrot.center_y])
-
-            # Beräkna avståndet till moroten som är närmast
-            nerest_carrot = [(carrot_cor[0][0] - self.center_x), (carrot_cor[0][1] - self.center_y)]
-            for carrot in carrot_cor:
-                if ((carrot[0] - self.center_x) ** 2 + (carrot[1] - self.center_y) ** 2) < (
-                        nerest_carrot[0] ** 2 + nerest_carrot[1] ** 2):
-                    nerest_carrot = [(carrot[0] - self.center_x), (carrot[1] - self.center_y)]
-
-            # Beräkna vinkel mot moroten
-            ang = math.atan2(nerest_carrot[1], nerest_carrot[0])
-            foodspeed = random.random() * self.finforce / self.mass
-            self.acc_x = foodspeed * math.cos(ang)
-            self.acc_y = foodspeed * math.sin(ang)
-
-    def random_move(self):
-        # Ändra accelerationen slumpartat
-        self.acc_x = (random.random() * 2 - 1) * self.finforce / self.mass
-        self.acc_y = (random.random() * 2 - 1) * self.finforce / self.mass
-
-    def check_edge(self):
-        # Kolla om fisken är nära kanten, styr in dem mot mitten och stressa upp den
-        if self.center_x > self.sw * 0.90:
-            self.acc_x = - self.finforce / self.mass
-            self.relaxed[0] = False
-        if self.center_x < self.sw * 0.10:
-            self.acc_x = self.finforce / self.mass
-            self.relaxed[0] = False
-
-        if self.center_y > self.sh * 0.90:
-            self.acc_y = - self.finforce / self.mass
-            self.relaxed[1] = False
-        if self.center_y < self.sh * 0.10:
-            self.acc_y = self.finforce / self.mass
-            self.relaxed[1] = False
 
 
     def animate(self):
