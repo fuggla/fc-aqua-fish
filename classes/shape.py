10510ff6f67aab2475d1066732ad494f16e87b35
Fred Uggla <fred.uggla@owlnical.net>
2018-12-27 11:33:27 +0100

    Klass Shape för att rita former

---
+++ classes/shape.py
@@ -0,0 +1,9 @@
+# Klass för att rita diverse former
+class Shape():
+
+    def __init__(self, x, y, width, height):
+        # Position och storlek
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
