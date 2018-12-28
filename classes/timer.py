56aa03946b4d423ad87c34a1263ff2c54a89500a
Fred Uggla <fred.uggla@owlnical.net>
2018-12-28 18:09:31 +0100

    Print när timer startas och är klar

--- classes/timer.py
+++ classes/timer.py
@@ -2,15 +2,19 @@
 import time
 
 class Performance_timer():
-    def __init__(self):
+    def __init__(self, output):
         self.timer_start = time.perf_counter()
         self.last = 0.0
+        print(output)
 
     # Print time elapsed since last print and total time since creation
     def print(self, output):
         now = float(self)
         print(output, "in: %.4fs" % float(now-self.last), "(total: %.4fs)" % now)
         self.last = now
+
+    def done(self, output):
+        print(output, "in", self)
 
     def __str__(self):
         return "%.4f" % (time.perf_counter()-self.timer_start)
