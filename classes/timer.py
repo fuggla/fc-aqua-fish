c7fb3736e79172d79bccbccbae9dfb49b8e2511b
Fred Uggla <fred.uggla@owlnical.net>
2018-12-28 17:46:08 +0100

    Klass för att mäta tid

---
+++ classes/timer.py
@@ -0,0 +1,19 @@
+# Class for measuring elapsed time between calls to self.print
+import time
+
+class Performance_timer():
+    def __init__(self):
+        self.timer_start = time.perf_counter()
+        self.last = 0.0
+
+    # Print time elapsed since last print and total time since creation
+    def print(self, output):
+        now = float(self)
+        print(output, "in: %.4fs" % float(now-self.last), "(total: %.4fs)" % now)
+        self.last = now
+
+    def __str__(self):
+        return "%.4f" % (time.perf_counter()-self.timer_start)
+
+    def __float__(self):
+        return float("%.4f" % (time.perf_counter()-self.timer_start))
