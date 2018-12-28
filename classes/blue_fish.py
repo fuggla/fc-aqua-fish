4afb29962f7b158db0b93fb94c73b84a2d88781b
JontePonte <john_nordstrand@hotmail.com>
2018-12-28 15:39:39 +0100

    blue_fish laddar nu in texture och position

--- classes/blue_fish.py
+++ classes/blue_fish.py
@@ -29,6 +29,27 @@ class BfishSprite(FishSprite):
         self.sh = SCREEN_HEIGHT
         self.food_objects = carrot_list
 
-        self.sw = SCREEN_WIDTH
-        self.sh = SCREEN_HEIGHT
-        self.food_objects = carrot_list
+        # texture 1 & 2 för höger och vänster
+        scale_factor = SPRITE_SCALING_BFISH * self.size / 8
+        if color == "green":
+            self.texture_left1 = arcade.load_texture("images/green_fish1.png", mirrored=True, scale=scale_factor)
+            self.texture_left2 = arcade.load_texture("images/green_fish2.png", mirrored=True, scale=scale_factor)
+            self.texture_right1 = arcade.load_texture("images/green_fish1.png", scale=scale_factor)
+            self.texture_right2 = arcade.load_texture("images/green_fish2.png", scale=scale_factor)
+        else:
+            self.texture_left1 = arcade.load_texture("images/blue_fish1.png", mirrored=True, scale=scale_factor)
+            self.texture_left2 = arcade.load_texture("images/blue_fish2.png", mirrored=True, scale=scale_factor)
+            self.texture_right1 = arcade.load_texture("images/blue_fish1.png", scale=scale_factor)
+            self.texture_right2 = arcade.load_texture("images/blue_fish2.png", scale=scale_factor)
+
+        # Slumpa fiskarna höger/vänster
+        if random.random() > 0.5:
+            self.texture = self.texture_left1
+            self.whichtexture = 11  # 11 = left1
+        else:
+            self.texture = self.texture_right1
+            self.whichtexture = 21  # 21 = right1
+
+        # Placera ut fiskarna
+        self.center_x = random.randrange(self.sw * 0.8) + self.sw * 0.1
+        self.center_y = random.randrange(self.sh * 0.8) + self.sh * 0.1
