8deac559df1927de4a3b41d578732158d8db33f3
Fred Uggla <fred.uggla@owlnical.net>
2019-01-03 15:44:47 +0100

    Använd height som skickas till MyGame()

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ class MyGame(arcade.Window, State):
         self.frame_count = 0
         self.show_windows = True
         self.background = None
-        self.center_cords = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        self.width_height = (SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.center_cords = (width // 2, height // 2)
+        self.width_height = (width, height)
 
         # Skapa shapes och sprites som <NAMN>_list
         self.sprite_list_names = [ "pfish", "bfish", "shark", "carrot", "blueberry", "plant_blueberry", "plant_foreground", "fish_egg", "all_sprite" ]
@@ -155,7 +155,7 @@ class MyGame(arcade.Window, State):
                 if hit_list and fish.isalive:
                     fish.eat_food(hit_list[0], 10)       # 10 är hur mycket de äter varje tugga
                 # Ta bort döda fiskar som flytit upp
-                if fish.bottom > SCREEN_HEIGHT and fish.health <= 0:
+                if fish.bottom > self.height and fish.health <= 0:
                     fish.kill()
                 # Lägg ägg ifall fisken är mätt
                 if fish.health > fish.base_health * 1.1 and fish.name_gender[1] == "f" and random.randrange(1000) < pfish_egg_freq:
@@ -181,7 +181,7 @@ class MyGame(arcade.Window, State):
                     fish.eat_food(hit_list[0], 1)  # 1 är hur mycket de äter varje tugga
 
                 # Ta bort döda fiskar som flytit upp
-                if fish.bottom > SCREEN_HEIGHT and fish.health <= 0:
+                if fish.bottom > self.height and fish.health <= 0:
                     fish.kill()
                 # Lägg ägg ifall fisken är mätt
                 if fish.health > fish.base_health * 1.1 and fish.name_gender[1] == "f" and random.randrange(1000) < bfish_egg_freq:
@@ -193,12 +193,12 @@ class MyGame(arcade.Window, State):
                 # Ätalgoritm för blue shark
                 if fish.iseating > 0:
                     fish.iseating -= 1
-                hit_list = arcade.check_for_collision_with_list(fish, self.bfish_list)
+                hit_list = check_for_collision_with_list(fish, self.bfish_list)
                 # Om fisken lever och det finns en blue small fish äter fisken den
                 if hit_list and fish.isalive:
                     fish.eat_fish(hit_list[0])
                 # Ta bort döda fiskar som flytit upp
-                if fish.bottom > SCREEN_HEIGHT and fish.health <= 0:
+                if fish.bottom > self.height and fish.health <= 0:
                     fish.kill()
                 # Lägg ägg ifall fisken är mätt
                 if fish.health > fish.base_health * 1.1 and fish.name_gender[1] == "f" and random.randrange(1000) < shark_egg_freq:
@@ -298,12 +298,12 @@ class MyGame(arcade.Window, State):
         fish = None
         if (name == "pfish"):
             color = ["purple", "orange", "green"]
-            fish = PfishSprite(self.carrot_list, color=color[random.randrange(3)], setpos_y=SCREEN_HEIGHT, setspeed_y=-30)
+            fish = PfishSprite(self.carrot_list, color=color[random.randrange(3)], setpos_y=self.height, setspeed_y=-30)
         elif (name == "bfish"):
-            fish = BfishSprite(self.carrot_list, self.blueberry_list, self.bfish_list, self.shark_list, setpos_y=SCREEN_HEIGHT, setspeed_y=-30)
+            fish = BfishSprite(self.carrot_list, self.blueberry_list, self.bfish_list, self.shark_list, setpos_y=self.height, setspeed_y=-30)
             self.bfish_list.append(fish)
         elif (name == "shark"):
-            fish = SharkSprite(self.bfish_list, setpos_y = SCREEN_HEIGHT, setspeed_y=-30, event=self.event)
+            fish = SharkSprite(self.bfish_list, setpos_y = self.height, setspeed_y=-30, event=self.event)
             self.shark_list.append(fish)
 
         # Done and done
@@ -332,7 +332,7 @@ class MyGame(arcade.Window, State):
         event.open()
 
         # Skapa meny för att interagera med akvariet
-        action= Window(60, SCREEN_HEIGHT / 2, 100, 170, " Store", title_height=20, title_align="left")
+        action= Window(60, self.height/ 2, 100, 170, " Store", title_height=20, title_align="left")
         action.add_button(10, 10, 80, 30, "Pfish", 11, self.buy_pfish)
         action.add_button(50, 10, 80, 30, "Bfish", 11, self.buy_bfish)
         action.add_button(90, 10, 80, 30, "Shark", 11, self.buy_shark)
