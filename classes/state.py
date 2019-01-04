f8db1a17670818b37c7a3d0e9c621fe39c8273cd
Fred Uggla <fred.uggla@owlnical.net>
2019-01-04 11:51:43 +0100

    Nytt state för startmeny

--- classes/state.py
+++ classes/state.py
@@ -1,21 +1,26 @@
 # Hantering av game state
 class State():
-    # Ändra läge mellan paused och playing
-    def toggle_pause(self):
-        self.state = "playing" if self.is_paused() else "paused"
+    # Startmenyn
+    def start(self):
+        self.state = "start"
 
-    # Ändra state till playing
+    # In game
     def play(self):
         self.state = "playing"
 
-    # Ändra state till playing
+    # In game paus
     def pause(self):
         self.state = "paused"
 
-    # Return true om spelet är pausat
     def is_paused(self):
         return True if self.state == "paused" else False
 
-    # Return true om spelet spelas
     def is_playing(self):
         return True if self.state == "playing" else False
+
+    def is_started(self):
+        return True if self.state == "start" else False
+
+    # Ändra läge mellan paused och playing
+    def toggle_pause(self):
+        self.state = "playing" if self.is_paused() else "paused"
