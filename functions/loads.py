b29c7a3cb84d0e91bd18eb4256a7e405eadf7bf1
Fred Uggla <fred.uggla@owlnical.net>
2019-01-18 22:07:54 +0100

    Fiskstatus och Hälsa i statistik

--- functions/loads.py
+++ functions/loads.py
@@ -82,8 +82,8 @@ def load_windows(game):
     )
 
     stats = Window(110, game.height - 80, 200, 130, " Stats", title_height=20, title_align="left")
-    stats_left = stats.add_text(15, 12, 120, 80, rows=6)
-    stats_right = stats.add_text(15, 132, 60, 80, rows=6, align="right")
+    stats_left = stats.add_text(15, 12, 120, 80, rows=8)
+    stats_right = stats.add_text(15, 132, 60, 80, rows=8, align="right")
 
     return [main, event, action, pause, stats], pause, eventhandler, stats_left, stats_right
 
