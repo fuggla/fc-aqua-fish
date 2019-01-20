e18a9beff3189982ce731cffb6ab0f0a41bfb1d9
JontePonte <john_nordstrand@hotmail.com>
2019-01-20 12:10:26 +0100

    La till klass för texten "Press Space to show menus"

---
+++ classes/press_space.py
@@ -0,0 +1,38 @@
+from arcade import draw_text
+from vars import SCREEN_WIDTH
+
+""" Skriv ut "press "space" to show menus" """
+
+
+class PressSpace:
+    def __init__(self, x=SCREEN_WIDTH/2, y=50):
+        self.x = x
+        self.y = y
+        self.tick = 0
+        self.timer = 0
+        self.enabled = False
+
+    def update(self):
+        # Öka timer varje sekund
+        if self.tick % 60 == 0 and self.show_text():
+            self.timer += 1
+        self.tick += 1
+
+        # Sudda ut efter fyra sekunder
+        if self.timer >= 4:
+            self.enabled = False
+
+        if self.show_text():
+            self.enabled = True
+            self.timer = 0
+
+        if self.enabled:
+            self.draw()
+
+        super().update()
+
+    def show_text(self):
+        self.enabled = True
+
+    def draw(self):
+        draw_text("Press Space to show menues", self.x, self.y, (0, 0, 0), font_size=12, width=10, align="left", anchor_x="center", anchor_y="center")
