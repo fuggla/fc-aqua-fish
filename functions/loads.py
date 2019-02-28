7e6cfffc10e99144f6315fa47d1e33c142648253
JontePonte <john_nordstrand@hotmail.com>
2019-02-28 22:04:11 +0100

    Fixade ljudformat. Alla utom fall_into_water_heavy fungerar (vet inte varför den krånglar...)

--- functions/loads.py
+++ functions/loads.py
@@ -38,11 +38,11 @@ def load_music():
 # Load sound effect files
 def load_sound_effect():
     files = [
-        "assets/sound_effects/ambient_bubble.mp3",
+        "assets/sound_effects/ambient_bubble.wav",
         "assets/sound_effects/eat.wav",
         "assets/sound_effects/egg.wav",
         "assets/sound_effects/fall_into_water_heavy.wav",
-        "assets/sound_effects/fall_into_water_light.aiff",
+        "assets/sound_effects/fall_into_water_light.wav",
         "assets/sound_effects/kiss.wav",
         "assets/sound_effects/pick_up_fish.wav",
     ]
