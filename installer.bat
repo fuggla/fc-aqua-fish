dd44783b52a45a2d8b0181e32e2017351e663586
owlnical <fred.uggla@owlnical.net>
2019-04-07 17:56:12 +0200

    Ersatte bat-installer med en ahk-installer

--- installer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pyinstaller --add-data assets;resources --add-data ./avbin64.dll;. --add-data ./avbin.dll;Win64 --onefile main.py fish_vars.py vars.py functions\diagnose_name_gender_attraction_health.py functions\diagnose_name_gender_health_hungry.py functions\loads.py classes\blue_small_fish.py classes\carrot.py classes\fish.py  classes\fps.py classes\plant_blueberry.py classes\popcorn.py classes\purple_fish.py classes\state.py classes\blueberry.py classes\egg_fish.py classes\fish_animate.py classes\hook.py  classes\plant_foreground.py classes\press_q.py classes\shape.py classes\timer.py classes\bubble_map.py classes\fade.py classes\fish_move.py  classes\liquid_assets.py classes\pointer.py  classes\press_space.py classes\shark.py classes\window.py
-pause
